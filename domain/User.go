db3049ca712a86efb0bdbdfd40c31e459f7f30c8
renatobaez <panchobaez@gmail.com>
2023-12-13 14:43:42 -0300

    first commit

---
+++ domain/User.go
@@ -0,0 +1,11 @@
+package domain
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	FirstName string `gorm:"not null" json:"first_name"`
+	LastName  string `gorm:"not null" json:"last_name"`
+	Email     string `gorm:"not null; unique_index" json:"email"`
+	Tasks     []Task `json:"tasks"`
+}
